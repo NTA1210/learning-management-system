ce2214bcd1d93df9bb1048f00dbce1f338389d6c
NTA1210 <anhntde180721@fpt.edu.vn>
2025-10-18 10:00:08 +0700

    refactor(auth) : modify send email  feature

--- BE_LMS/src/utils/sendMail.ts
+++ BE_LMS/src/utils/sendMail.ts
@@ -1,5 +1,6 @@
+import { get } from "node:http";
 import resend from "../config/resend";
-import { EMAIL_SENDER } from "../constants/env";
+import { EMAIL_SENDER, NODE_ENV } from "../constants/env";
 
 type Params = {
   to: string;
@@ -10,10 +11,17 @@ type Params = {
 
 export type SendMailResult = { error?: string };
 
+const getFromMail = () => {
+  return NODE_ENV === "development" ? "anhkn7@gmail.com" : EMAIL_SENDER;
+};
+
+const getToEmail = (to: string) => {
+  return NODE_ENV === "development" ? "delivered@resend.dev" : EMAIL_SENDER;
+};
 export const sendMail = async ({ to, subject, text, html }: Params) => {
   await resend.emails.send({
-    from: EMAIL_SENDER,
-    to,
+    from: getFromMail(),
+    to: getToEmail(to),
     subject,
     text,
     html,
