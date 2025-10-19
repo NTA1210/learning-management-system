6366d12e91badcf2435f4313ddb50a95d4341f28
NTA1210 <anhntde180721@fpt.edu.vn>
2025-10-19 11:48:03 +0700

    feat(auth):get and delete sessions feature

---
+++ BE_LMS/src/routes/session.route.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import {
+  deleteSessionHandler,
+  getSessionHandler,
+} from "../controller/session.controller";
+
+const sessionRoutes = Router();
+
+//prefix: /sessions
+sessionRoutes.get("/", getSessionHandler);
+sessionRoutes.delete("/:id", deleteSessionHandler);
+
+export default sessionRoutes;
