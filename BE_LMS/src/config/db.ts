2d966398a5382dcbee81f1903cbf50ffbc4199e3
NTA1210 <anhntde180721@fpt.edu.vn>
2025-10-20 12:05:17 +0700

    refactor(schema): delete env var

--- BE_LMS/src/config/db.ts
+++ BE_LMS/src/config/db.ts
@@ -4,9 +4,9 @@ import { MONGO_URI } from "../constants/env";
 const connectToDatabase = async () => {
   try {
     await mongoose.connect(MONGO_URI);
-    console.log("Connected to database", MONGO_URI);
+    console.log("Connected to database");
   } catch (error) {
-    console.log("Could not connect to database", MONGO_URI);
+    console.log("Could not connect to database");
     process.exit(1);
     //shutdown server if connection fails
   }
