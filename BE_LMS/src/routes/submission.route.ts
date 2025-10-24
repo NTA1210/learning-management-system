812ef0a89daf06e496fe75d35718ccb5e00bf9b9
tienhungnguyen <tienhung1201@gmail.com>
2025-10-24 16:41:13 +0700

    refactor authorize,submissions endpoit

--- BE_LMS/src/routes/submission.route.ts
+++ BE_LMS/src/routes/submission.route.ts
@@ -6,13 +6,18 @@ import {
     listSubmissionsByAssignmentHandler,
 }from "../controller/submission.controller";
 import  authenticate  from "../middleware/authenticate";
+import { Role } from "@/types";
+import authorize from "@/middleware/authorize";
+import sessionRoutes from "./session.route";
+
+
 
 const submissionRoutes = Router();
 
 // prefix: /submissions
 
 //sv nộp bài
-submissionRoutes.post("/:assignmentId", authenticate, submitAssignmentHandler);
+submissionRoutes.post("/:assignmentId", authenticate,submitAssignmentHandler);
 
 //sv nộp lại bài (khi được phép)
 submissionRoutes.put("/:assignmentId/resubmit", authenticate, resubmitAssignmentHandler);
@@ -21,6 +26,6 @@ submissionRoutes.put("/:assignmentId/resubmit", authenticate, resubmitAssignment
 submissionRoutes.get("/:assignmentId/status", authenticate, getSubmissionStatusHandler);
 
 //GV xem danh sách bài nộp của 1 assignment
-submissionRoutes.get("/:assignmentId/all", authenticate, listSubmissionsByAssignmentHandler);
+submissionRoutes.get("/:assignmentId/all", authenticate,authorize(Role.ADMIN,Role.TEACHER), listSubmissionsByAssignmentHandler);
 
 export default submissionRoutes;
