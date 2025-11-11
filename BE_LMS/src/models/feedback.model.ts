910f1db9fa749dd19cf4b7599be8aac27a410c92
Nguyễn Anh <anhkn7@gmail.com>
2025-11-11 16:19:23 +0700

    refactor(feedback): modify feedback model

--- BE_LMS/src/models/feedback.model.ts
+++ BE_LMS/src/models/feedback.model.ts
@@ -9,13 +9,28 @@ const feedbackSchema = new mongoose.Schema<IFeedback>(
       enum: FeedbackType,
       required: true,
     },
-    content: { type: String, required: true },
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    rating: { type: Number, required: true },
     userId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
     },
     targetId: { type: mongoose.Schema.Types.ObjectId }, // có thể ref tới Teacher, Course …
+    originalName: { type: String },
+    mimeType: { type: String },
+    key: { type: String },
+    size: {
+      type: Number,
+      default: 0,
+      validate: {
+        validator: function (v) {
+          return v <= 20 * 1024 * 1024;
+        },
+        message: "File size must be <= 20MB",
+      },
+    },
   },
   { timestamps: true }
 );
--- BE_LMS/src/types/feedback.type.ts
+++ BE_LMS/src/types/feedback.type.ts
@@ -3,15 +3,20 @@ import mongoose from "mongoose";
 export enum FeedbackType {
   SYSTEM = "system",
   TEACHER = "teacher",
-  COURSE = "course",
   OTHER = "other",
 }
 
 export default interface IFeedback extends mongoose.Document {
   type: FeedbackType;
-  content: string;
+  title: string;
+  description: string;
+  rating: number;
   userId: mongoose.Types.ObjectId;
   targetId?: mongoose.Types.ObjectId; // ví dụ teacherId hoặc courseId
+  originalName?: string;
+  mimeType?: string;
+  key?: string;
+  size?: number;
   createdAt?: Date;
   updatedAt?: Date;
 }
