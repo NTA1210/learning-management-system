164a4067fdfb8bff7a5b7a74d3837118e80d868f
Nguyễn Anh <anhkn7@gmail.com>
2025-11-18 15:29:09 +0700

    get users

--- BE_LMS/src/services/user.service.ts
+++ BE_LMS/src/services/user.service.ts
@@ -1,5 +1,3 @@
-import { NOT_FOUND } from "@/constants/http";
-import courseModel from "@/models/course.model";
 import EnrollmentModel from "@/models/enrollment.model";
 import UserModel from "@/models/user.model";
 import { Role, UserStatus } from "@/types";
@@ -7,13 +5,9 @@ import appAssert from "@/utils/appAssert";
 import { TGetAllUsersFilter } from "@/validators/user.schemas";
 
 export const getAllUsers = async (
-  courseId: string,
   request: TGetAllUsersFilter,
   viewerRole: Role
 ) => {
-  const course = await courseModel.findById(courseId);
-  appAssert(course, NOT_FOUND, "Course not found");
-
   const { role, email, username, page = 1, limit = 10 } = request;
 
   // 3️⃣ Pagination
@@ -23,57 +17,31 @@ export const getAllUsers = async (
 
   if (viewerRole === Role.ADMIN) {
     if (role) query.role = role; // admin có thể lọc role cụ thể
-  } else {
-    if (role && role !== Role.ADMIN) {
-      query.role = role;
-    } else {
-      query.role = { $ne: Role.ADMIN };
-    }
+  }
+  if (viewerRole === Role.TEACHER) {
+    query.role = Role.STUDENT; // teacher chỉ được xem student
   }
 
   if (email) query.email = { $regex: email, $options: "i" };
   if (username) query.username = { $regex: username, $options: "i" };
 
-  const [users, enrolledList] = await Promise.all([
-    UserModel.find({
-      ...query,
-      verified: true,
-      status: UserStatus.ACTIVE,
-    })
-      .sort({ createdAt: -1 })
+  const [users, total] = await Promise.all([
+    UserModel.find(query)
       .skip(skip)
       .limit(limit)
+      .select("-password -__v")
       .lean(),
-    EnrollmentModel.find({ courseId, status: "active" }).select("userId"),
+    UserModel.countDocuments(query),
   ]);
 
-  const enrolledUerIds = new Set<String>(
-    enrolledList.map((enrollment) => enrollment.studentId.toString())
-  );
-
-  const usersWithEnrolledStatus = users.map((user) => {
-    const isEnrolled = enrolledUerIds.has(user._id.toString());
-    return {
-      ...user,
-      isEnrolled,
-    };
-  });
-
-  const formattedUsers = usersWithEnrolledStatus.map((u) =>
-    formatUserResponse(u, viewerRole)
-  );
-
-  const total = await UserModel.countDocuments({
-    ...query,
-    status: UserStatus.ACTIVE,
-  });
+  // Kiểm tra và đánh dấu người dùng đã đăng ký khóa học (nếu viewer là student)
 
   const totalPages = Math.ceil(total / limit);
   const hasNext = page < totalPages;
   const hasPrev = page > 1;
 
   return {
-    users: formattedUsers,
+    data: users.map((user) => formatUserResponse(user, viewerRole)),
     pagination: {
       total,
       page,
