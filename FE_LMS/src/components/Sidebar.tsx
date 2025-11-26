d93727689ff3bbc47fbb8494a6a406f18ca7282c
nguyentoann <cuutoan.nguyen@gmail.com>
2025-11-26 13:33:35 +0700

    fix(sidebar): fix sidebar

--- FE_LMS/src/components/Sidebar.tsx
+++ FE_LMS/src/components/Sidebar.tsx
@@ -61,7 +61,8 @@ const getMenuItems = (role: 'admin' | 'teacher' | 'student'): MenuItem[] => {
     },
   ];
 
-  baseItems.push({
+  baseItems.push(
+    {
       href: "/quizz",
       icon: (
         <svg className="w-5 h-5 min-w-[1.25rem]" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -74,13 +75,12 @@ const getMenuItems = (role: 'admin' | 'teacher' | 'student'): MenuItem[] => {
       href: "/forum-list",
       icon: (
         <svg className="w-5 h-5 min-w-[1.25rem]" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"></path>
-      </svg>
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"></path>
+        </svg>
       ),
       label: "Forum"
     }
-
-  ];
+  );
 
   if (role === 'admin') {
     return [
